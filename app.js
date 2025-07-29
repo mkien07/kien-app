e52fb069dc74500fba867609abf73b6d57bd28fe
mkien07 <mkien0119@gmail.com>
2025-07-29 13:20:20 +0700

    fix

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ const userSchema = new mongoose.Schema({
   vipLevel: { type: String, default: 'VIP1' },
   role: { type: String, default: 'user' }
 });
-const User = mongoose.model('User', userSchema);
+//const User = mongoose.model('User', userSchema);
 
 const withdrawSchema = new mongoose.Schema({
   userId: String,
